c61f57efaab95a9bf196e97bdff34b173d60987b
Dylan <312742568@qq.com>
2018-11-09 18:46:48 +0800

    合作伙伴详情(3)

--- element_ui_test/src/pages/base_partner_detail.vue
+++ element_ui_test/src/pages/base_partner_detail.vue
@@ -48,23 +48,18 @@
 			  	<el-breadcrumb-item :to="{ path: '/partnerList' }">合作伙伴</el-breadcrumb-item>
 			  	<el-breadcrumb-item>
 			  		<template>
-			  			<p>详情</p>
+			  			<p v-text="partnerName"></p>
 			  		</template>
 			  	</el-breadcrumb-item>
 			</el-breadcrumb>
 			<el-tabs v-model="message">
     			<el-tab-pane label="基础资料" name="first">
     				<ul class="tab-ul">
-    					<li class="firstItem">基础信息</li>
-    					<li>
-    						<h2>ID</h2>
-    						<p>123456</p>
-    						<a href="javascript:void(0)">修改</a>
+    					<li class="{firstItem:index==0}" v-for="(item,index) in baseDetail" :key="index">
+    						<h2 v-text="item.name"></h2>
+    						<p v-text="item.value"></p>
+    						<a v-if="index>1" href="javascript:void(0)">修改</a>
     					</li>
-    					<li>基础资料</li>
-    					<li>基础资料</li>
-    					<li>基础资料</li>
-    					<li>基础资料</li>
     				</ul>
     			</el-tab-pane>
     			<el-tab-pane label="充电站点" name="second">
@@ -85,10 +80,102 @@
 	</div>
 </template>
 <script>
+	import {httpRequest} from '../assets/js/httpRequest.js'
 	export default {
 		data() {
 			return {
-				message: 'first'
+				message: 'first',
+				partnerId: this.$route.query.partnerId,
+				partnerName: '',
+				partnerDetail: '',
+				partnerManager: '',
+				baseArray: [
+					'基础信息',
+					'ID：',
+					'名称：',
+					'类型：',
+					'结算周期：',
+					'分成方式：',
+					'分成比例：',
+					'度数分成：',
+					'4S店优惠：',
+					'公司主体：',
+					'优惠时间：',
+					'开户银行：',
+					'开户城市：',
+					'银行户名：',
+					'银行账户：',
+					'邮箱地址：',
+					'管理员：',
+					'管理员电话：',
+					'备注：'
+				],
+				baseDetail: []
+			}
+		},
+		created() {
+			this.getPartnerDetailInfo();
+		},
+		watch: {
+
+		},
+		methods: {
+			getPartnerDetailInfo() {
+				let self = this;
+				httpRequest({
+					url: 'findPartnerById.json',
+					method: 'GET',
+					data:{
+						partnerId: self.partnerId
+					}
+				}).then((res) => {
+					self.partnerDetail = res;
+					self.partnerName = '详情('+res.partnerName+')';
+					self.getPartnerManagerInfo(res);
+				})
+			},
+			getPartnerManagerInfo(base) {
+				let self = this;
+				httpRequest({
+					url: 'getPartnerManagerInfo.json',
+					method: 'GET',
+					data: {
+						partnerId: self.partnerId
+					}
+				}).then((res) => {
+					self.partnerManager = res;
+					var baseData = [
+						'',
+						base.partnerID,
+						base.partnerName,
+						base.partnerType,
+						base.timeCycle,//结算周期
+						base.settlementType,//分成方式
+						base.proration,//分成比例
+						base.unit,//度数分成
+						base.discountFlag,//4S店优惠
+						base.xlvrenType,//公司主体
+						base.discountStartDate,//优惠时间
+						base.bank,//开户银行
+						base.bankCityName,//开户城市
+						base.bankUserName,//银行户名
+						base.bankAccount,//银行账户
+						base.email,//邮箱地址
+						res.username,//管理员
+						res.userMobile,//管理员电话
+						base.comment//备注
+					];
+					self.dataStringifyToJson(baseData);
+				})
+			},
+			dataStringifyToJson(arr) {
+				let self = this;
+				for (var i = 0; i < self.baseArray.length; i++) {
+					self.baseDetail.push({
+						name: self.baseArray[i],
+						value: arr[i]
+					})
+				}
 			}
 		}
 	}
